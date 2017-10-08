0dd46677e8253265eeb485f55e3ff8e96977457f
Damian Krzeminski <pirxpilot@code42day.com>
2017-10-07 19:17:14 -0600

    initial implementaion

--- test/zaraz.js
+++ test/zaraz.js
@@ -1,9 +1,81 @@
-var should = require('should');
 var zaraz = require('../');
 
 describe('zaraz node module', function () {
-  it('must have at least one test', function () {
-    zaraz();
-    should.fail('Need to write tests.');
+  it('must call callback with params', function (done) {
+
+    zaraz(function(a, b) {
+      a.should.be.eql(5);
+      b.should.be.eql(-3);
+      done();
+    }, 5, -3);
   });
+
+  it('must call callbacks in order', function(done) {
+    var r = '';
+    function fn(p) {
+      r += p;
+    }
+
+    zaraz(fn, 'A');
+    zaraz(fn, 'B');
+    zaraz(fn, 'C');
+    zaraz(function() {
+      r.should.be.eql('ABC');
+      done();
+    });
+  });
+
+  it('must allow to clear a callback', function(done) {
+    var r = '';
+    function fn(p) {
+      r += p;
+    }
+
+    zaraz(fn, 'A');
+    var b = zaraz(fn, 'B');
+    zaraz(fn, 'C');
+    zaraz(function() {
+      r.should.be.eql('AC');
+      done();
+    });
+    b.clear();
+  });
+
+  it('must allow for running a callback manually', function(done) {
+    var r = '';
+    function fn(p) {
+      r += p;
+    }
+
+    zaraz(fn, 'A');
+    var b = zaraz(fn, 'B');
+    zaraz(fn, 'C');
+    zaraz(function() {
+      r.should.be.eql('BAC');
+      done();
+    });
+    b.run();
+  });
+
+  it('must pospone callbacks scheduled during callback', function(done) {
+    var r = '';
+    function fn(p) {
+      r += p;
+    }
+
+    zaraz(fn, 'A');
+    zaraz(function() {
+      fn('B');
+      zaraz(fn, 'D');
+      zaraz(function() {
+        r.should.be.eql('ABCD');
+        done();
+      });
+    });
+    zaraz(fn, 'C');
+    zaraz(function() {
+      r.should.be.eql('ABC');
+    });
+  });
+
 });
